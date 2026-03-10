fa6608a2ff3854f5ec20976191d59649f5ba2673
Din N <runniddefault@gmail.com>
2026-03-10 22:13:31 +0600

    fix: set default pagination class to hide drf warnings

--- library_api/settings.py
+++ library_api/settings.py
@@ -140,6 +140,7 @@ REST_FRAMEWORK = {
         'anon': '100/day',
         'user': '10/hour'
     },
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20
 }
 
